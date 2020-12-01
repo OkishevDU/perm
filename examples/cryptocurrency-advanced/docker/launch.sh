38f0c620c91c5077ed9443afe15147767dbc8ef6
OkishevDU <masterche0@mail.ru>
2020-12-01 14:10:45 +0500

    New

--- examples/cryptocurrency-advanced/docker/launch.sh
+++ examples/cryptocurrency-advanced/docker/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-node_count=4
+node_count=5
 start_peer_port=6331
 start_public_port=8000
 path_to_app=/root/.cargo/bin/exonum-cryptocurrency-advanced
@@ -18,7 +18,7 @@ for i in $(seq 0 $((node_count - 1)))
 do
   public_port=$((start_public_port + i))
   private_port=$((public_port + node_count))
-  ${path_to_app} finalize --public-api-address 0.0.0.0:${public_port} --private-api-address 0.0.0.0:${private_port} $((i + 1))/sec.toml $((i + 1))/node.toml --public-configs {1,2,3,4}/pub.toml
+  ${path_to_app} finalize --public-api-address 0.0.0.0:${public_port} --private-api-address 0.0.0.0:${private_port} $((i + 1))/sec.toml $((i + 1))/node.toml --public-configs {1,2,3,4,5}/pub.toml
 done
 
 for i in $(seq 0 $((node_count - 1)))
